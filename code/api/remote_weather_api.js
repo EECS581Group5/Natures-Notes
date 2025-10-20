897fa5fc714757a436acfa87128f1ab43ec474e8
willbhedges <willbhedges@icloud.com>
2025-10-20 14:27:25 -0500

    give more access control

--- code/api/remote_weather_api.js
+++ code/api/remote_weather_api.js
@@ -1,4 +1,9 @@
 module.exports = async (req, res) => {
+
+    // CORS — allow your local dev and (optionally) others
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // or '*' for wide-open
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   const { lat, lon } = req.query;
   const key = process.env.REACT_APP_WEATHER_KEY; // only lives on Vercel
 
