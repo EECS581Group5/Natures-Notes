81c5ae030b19e353a42e62ed006d8087c0301d56
ruth-higgason <rhiggason@ku.edu>
2025-10-31 15:43:03 -0500

    change guest log out banner

--- code/src/components/Home.js
+++ code/src/components/Home.js
@@ -36,15 +36,10 @@ function Home() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>Nature Notes is coming soon!</p>
       </div>
-      <div className="clear"></div>
-
+      
+      <div className='guestLogOut'>
       {user && (
-        <div style={{
-          textAlign: 'center',
-          padding: '20px',
-          background: '#f5f5f5',
-          borderBottom: '1px solid #ddd'
-        }}>
+        <div>
           <p style={{ margin: '0 0 10px 0', color: '#333' }}>
             Welcome back, <strong>{user.username}</strong>!
           </p>
@@ -63,8 +58,9 @@ function Home() {
           >
             Logout
           </button>
-        </div>
-      )}
+        </div>)}
+      </div>
+      
             <header className="App-header">
         <Weather />
         <br />
